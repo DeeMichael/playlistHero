bcacab3f072f96f6c1796beec6ce68f26d686d7a
Michael Dee <michael.dee@state.co.us>
2018-05-14 14:42:12 -0600

    Added enter key triggering a search. Added onBlur and onFocus placeholder functionality

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -12,6 +12,9 @@ class SearchBar extends React.Component {
 
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleEnterKey = this.handleEnterKey.bind(this);
+    this.handleOnFocus = this.handleOnFocus.bind(this);
+    this.handleOnBlur = this.handleOnBlur.bind(this);
   }
 
   search(){
@@ -22,10 +25,30 @@ class SearchBar extends React.Component {
     this.setState({term: event.target.value});
   }
 
+  handleEnterKey(event){
+    if (event.keyCode === 13) {
+      this.search();
+    }
+  }
+
+  handleOnFocus(event){
+    let input = event.target;
+    if (!input.value) {
+      input.setAttribute("placeholder","");
+    }
+  }
+
+  handleOnBlur(event){
+    let input = event.target;
+    if (!input.value) {
+      input.setAttribute("placeholder","Enter A Song, Album, or Artist");
+    }
+  }
+
   render(){
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyUp={this.handleEnterKey} onFocus={this.handleOnFocus} onBlur={this.handleOnBlur} />
         <a onClick={this.search} >SEARCH</a>
       </div>
     );
